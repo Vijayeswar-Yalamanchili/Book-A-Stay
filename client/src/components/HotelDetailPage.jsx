e442945de42b15d8faad67cef64ad7e22b15aa5b
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-06-26 15:10:01 +0530

    updated with detail page

--- client/src/components/HotelDetailPage.jsx
+++ client/src/components/HotelDetailPage.jsx
@@ -6,7 +6,6 @@ import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 import { Link, useParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import hotelRoomImg from '../assets/hotelroom.jpeg'
-import { SharedDataContext } from '../contextApi/SharedDataComponent'
 import AxiosService from '../utils/AxiosService'
 import ApiRoutes from '../utils/ApiRoutes'
 
@@ -16,6 +15,7 @@ function HotelDetailPage() {
     let {id} = useParams()
     const [hotelData,setHotelData] = useState('')
     const [roomImgs,setRoomImgs] = useState([])
+    const [aminities,setAminities] = useState([])
     const getLoginToken = localStorage.getItem('loginToken')
 
     const getHotelData = async() => {
@@ -23,6 +23,7 @@ function HotelDetailPage() {
             let res = await AxiosService.get(`${ApiRoutes.HOTELBYID.path}/find/${id}`,{ headers : { 'Authorization' : `${getLoginToken}`} })
             let result = res.data.getHotelData
             setRoomImgs(result.roomImages)
+            setAminities(result.aminities)
             setHotelData(result)            
         } catch (error) {
             toast.error(error.response.data.message || error.message)
@@ -50,7 +51,6 @@ function HotelDetailPage() {
             <div className="roomImagesList my-4">
                 <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 4}}>
                     <Masonry gutter='0.5rem'>
-                        {/* {console.log(hotelData?.roomImages)} */}
                         {
                             roomImgs.length > 0 ?
                             roomImgs.map((e,i) => {
@@ -60,21 +60,21 @@ function HotelDetailPage() {
                         }
                     </Masonry>
                 </ResponsiveMasonry>
-                {/* <Row xs={1} className='mx-auto cityRows'>
-                {
-                    hotelData.roomImages.map((e,i) => {
-                        return <Card key={i} style={{width : "20rem"}} className='p-0'>
-                            <Card.Img src={`http://localhost:7000/${e.roomImages}`} className='roomViewImg'/>
-                        </Card>
-                    })
-                }
-                </Row> */}
             </div>
 
             <div className='desc d-flex justify-content-between align-items-center'>
                 <div className='rightDesc px-2 py-2'>
                     <h3>Stay In {hotelData.name}</h3>
-                    <p>{hotelData.overview}</p>
+                    <p className='d-flex flex-column'>
+                        {hotelData.overview}
+                        <div className='d-flex flex-row mt-2' style={{gap : "5px"}}> Aminities we have : 
+                            {
+                                aminities.map((ele,i)=> {
+                                    return <div className='label' key={i}>{ele}</div>
+                                })
+                            }
+                        </div>
+                    </p>
                 </div>
                 <div className='leftDesc  d-flex flex-column justify-content-between align-items-start'>
                     <div className='leftDescData px-4 py-3 d-flex flex-column justify-content-between align-items-start'>
