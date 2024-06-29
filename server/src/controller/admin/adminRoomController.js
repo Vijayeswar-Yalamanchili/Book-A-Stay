e8e17e545c9b4a1fa5a970db2da71c8416113f3e
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2024-06-29 23:19:05 +0530

    Updated with addRoom

--- server/src/controller/admin/adminRoomController.js
+++ server/src/controller/admin/adminRoomController.js
@@ -2,11 +2,20 @@ import RoomsModel from "../../models/roomsModel.js"
 
 const addRoom = async(req,res) => {
     try {
-        console.log(req.body,req.params)
-        const newRoom = await RoomsModel.create(req.body)
-        res.status(200).send({
-            newRoom 
-        })
+        const isHotelIdExists = await RoomsModel.find({ "$and" : [ {hotelId : req.params.hotelId}, {price : req.body.price} ]})
+        if(isHotelIdExists.length > 0){
+            const newRoom =  await RoomsModel.findOneAndUpdate({price : req.body.price}, {$push : {roomNumbers : req.body.roomNumbers}})
+            res.status(200).send({
+                message:"Room number added",
+                newRoom 
+            })
+        }else{
+            const newRoom = await RoomsModel.create({...req.body,hotelId : req.params.hotelId})
+            res.status(200).send({
+                message:"Room number added",
+                newRoom 
+            })
+        }
     } catch (error) {
         res.status(500).send({
             message:"Internal Server Error in Adding stay"
